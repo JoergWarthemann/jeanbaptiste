138df1ac4a9657a05eba08f11cbeb01ecfca8ca0
JoergWarthemann <joergwarthemann@hotmail.de>
2018-06-29 14:01:44 +0200

    * start implementation of sine tests

---
+++ JeanBaptiste.Test/src/FixtureSinCos.cpp
@@ -0,0 +1,50 @@
+#include <algorithm>
+#include <boost/math/constants/constants.hpp>
+#include <boost/test/unit_test.hpp>
+#include <cmath>
+#include <complex>
+#include <iostream>
+#include "../../JeanBaptiste/include/basic/SineCosine.h"
+#include <memory>
+#include <string>
+
+namespace constants = boost::math::constants;
+namespace jb = jeanbaptiste;
+namespace jbb = jeanbaptiste::basic;
+namespace ut = boost::unit_test;
+
+class SinCosFixture
+{
+public:
+    SinCosFixture()
+    {}
+
+    ~SinCosFixture()
+    {}
+};
+
+
+BOOST_FIXTURE_TEST_SUITE(SinCosTestSuite, SinCosFixture)
+
+    BOOST_AUTO_TEST_CASE(float_sine_from_minus_2pi_to_plus_2_pi)
+    {
+        BOOST_TEST_MESSAGE("Running sine computation in float range [-2pi ... 2pi].");
+
+        for (float x = -constants::two_pi<float>(); x <=constants::two_pi<float>(); x += 0.1)
+            BOOST_TEST(std::fabs(std::sin(x) - jbb::sine<float>(x)) < 0.00001);
+    }
+
+    BOOST_AUTO_TEST_CASE(double_sine_from_minus_2pi_to_plus_2_pi)
+    {
+        BOOST_TEST_MESSAGE("Running sine computation in double range [-2pi ... 2pi].");
+
+        for (double x = -constants::two_pi<double>(); x <=constants::two_pi<double>(); x += 0.1)
+            BOOST_TEST(std::fabs(std::sin(x) - jbb::sine<double>(x)) < 0.000000000001);
+    }
+
+    BOOST_AUTO_TEST_CASE(wait)
+    {
+        std::cin.get();
+    }
+
+BOOST_AUTO_TEST_SUITE_END()
--- JeanBaptiste.Test/src/test.cpp
+++ JeanBaptiste.Test/src/test.cpp
@@ -1,7 +1,8 @@
 #define BOOST_TEST_MODULE jeanbaptiste tests
 #define BOOST_TEST_DYN_LINK
 
-#include "FixtureRadix2.cpp"
-#include "FixtureRadix4.cpp"
-#include "FixtureRadixSplit24.cpp"
-#include "FixtureBitReversal.cpp"
+//#include "FixtureRadix2.cpp"
+//#include "FixtureRadix4.cpp"
+//#include "FixtureRadixSplit24.cpp"
+//#include "FixtureBitReversal.cpp"
+#include "FixtureSinCos.cpp"
